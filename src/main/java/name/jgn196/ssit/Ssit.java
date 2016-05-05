9f64c749307715872cbae63a26b5b2ef73324ac6
John Nash <john.nash@capgemini.com>
2016-05-05 10:54:26 +0100

    Cleaned up the Ssit class.

--- src/main/java/name/jgn196/ssit/Ssit.java
+++ src/main/java/name/jgn196/ssit/Ssit.java
@@ -1,14 +1,8 @@
 package name.jgn196.ssit;
 
-import java.io.File;
-import java.util.ArrayList;
-
 public class Ssit {
 
-    public static final String TODO_DIRECTORY_NAME = ".todo";
-
     private static final IssueStore issues = new InMemoryIssues();
-    private static final File TODO_DIRECTORY = new File(TODO_DIRECTORY_NAME);
 
     public static void main(final String[] args) {
         if (args.length == 0) return; // TODO - Print usage

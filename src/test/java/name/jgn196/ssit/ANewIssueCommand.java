ec88fcccf0f83fae035d2d7799c46f079871b0ba
John Nash <jnash@whiteclarkegroup.com>
2016-07-05 14:26:23 +0100

    Replaced MockStore class with Mockito.

--- src/test/java/name/jgn196/ssit/ANewIssueCommand.java
+++ src/test/java/name/jgn196/ssit/ANewIssueCommand.java
@@ -1,6 +1,7 @@
 package name.jgn196.ssit;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,23 +11,24 @@ public class ANewIssueCommand extends StandardOutCapturingTests {
     private static final String ISSUE_DESCRIPTION = "foo";
     private static final String[] VALID_ARGUMENTS = new String[]{"todo", ISSUE_DESCRIPTION};
 
+    private final IssueStore mockStore = Mockito.mock(IssueStore.class);
+
     @Test
     public void printsUsageIfIncorrectNumberOfArgumentsSupplied() {
-        assertThat(standardOutFrom(new NewIssueCommand(INVALID_ARGUMENTS, () -> null)))
+        assertThat(standardOutFrom(new NewIssueCommand(INVALID_ARGUMENTS, () -> mockStore)))
                 .startsWith("No new issue description provided");
     }
 
     @Test
     public void createsANewIssueFromASuppliedDescription() {
-        final MockStore issueStore = new MockStore();
+        new NewIssueCommand(VALID_ARGUMENTS, () -> mockStore).run();
 
-        new NewIssueCommand(VALID_ARGUMENTS, () -> issueStore).run();
-
-        assertThat(issueStore.newIssueDescriptions()).contains(ISSUE_DESCRIPTION);
+        Mockito.verify(mockStore).newIssue(ISSUE_DESCRIPTION);
     }
 
     @Test
     public void printsIssueCreationSuccess() {
-        assertThat(standardOutFrom(new NewIssueCommand(VALID_ARGUMENTS, MockStore::new))).isEqualTo("Issue added.");
+        assertThat(standardOutFrom(new NewIssueCommand(VALID_ARGUMENTS, () -> mockStore)))
+                .isEqualTo("Issue added.");
     }
 }

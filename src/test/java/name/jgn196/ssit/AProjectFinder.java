46b2a8b60fff78bf438b58be1d8369520dac41ff
John Nash <jnash@domain.whiteclarkegroup.com>
2016-07-05 09:07:46 +0100

    Added a placeholder test for a ProjectFinder class.

---
+++ src/test/java/name/jgn196/ssit/AProjectFinder.java
@@ -0,0 +1,14 @@
+package name.jgn196.ssit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class AProjectFinder {
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void findsTheSsitProjectDirectoryInTheSearchRootDirectory() {
+
+
+    }
+}
